d02f3e631bb17e3d10c1d84bb7487641351c9051
muhdfirdaus <dausmomo5779@gmail.com>
2020-07-28 16:29:17 +0800

    add SN checking features

---
+++ pages/check_cfg.php
@@ -0,0 +1,184 @@
+<?php session_start();
+// if(empty($_SESSION['id'])):
+// header('Location:../index.php');
+// endif;
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Box Configuration | <?php include('../dist/includes/title.php'); include('product_cfg.php');?></title>
+    <!-- Tell the browser to be responsive to screen width -->
+    <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
+    <!-- Bootstrap 3.3.5 -->
+    <link rel="stylesheet" href="../bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="../plugins/datatables/dataTables.bootstrap.css">
+    <link rel="stylesheet" href="../dist/css/AdminLTE.min.css">
+    <!-- AdminLTE Skins. Choose a skin from the css/skins
+         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="../dist/css/skins/_all-skins.min.css">
+    <style>
+      
+    </style>
+ </head>
+  <!-- ADD THE CLASS layout-top-nav TO REMOVE THE SIDEBAR. -->
+  <body class="hold-transition skin-black layout-top-nav">
+    <div class="wrapper">
+      <?php
+      include('../dist/includes/dbcon.php');
+      ?>
+      <!-- Full Width Column -->
+      <div class="content-wrapper">
+        <div class="container">
+          <!-- Content Header (Page header) -->
+          <section class="content-header">
+            <h1>
+              <a class="btn btn-lg btn-warning" href="sncheck.php">Back</a>
+              
+            </h1>
+            <ol class="breadcrumb">
+              <li><a href="box.php"><i class="fa fa-dashboard"></i> Home</a></li>
+              <li class="active">Box Configuration</li>
+            </ol>
+          </section>
+        <?php
+        $query=mysqli_query($con,"select * from box_config where id=1")or die(mysqli_error());
+        $row=mysqli_fetch_array($query);
+        $qty = $row['qty'];
+        $id = $row['id'];
+        $model = $row['model'];
+
+		    ?>	
+          <!-- Main content -->
+          <section class="content">
+            <div class="row">
+              <div class="col-md-12">
+                <div class="box box-primary">
+                  <div class="box-header">
+                    <h3 class="box-title">Box Configuration</h3>
+                  </div>
+                  <div class="box-body">
+                    <!-- Date range -->
+                    <form id = "formE" method="post" action="check_cfg_upd.php">
+    
+                    <div class="form-group">
+                      <label for="date">Product Name</label>
+                      <div class="input-group col-md-6">
+                        <select name="model" class="form-control">
+                          <?php $modellist = get_modelNo2($con);
+
+                          foreach($modellist as $key=>$value ){
+                          ?>
+                          <option value='<?php echo $key;?>'<?php if($model==$key){echo "selected"; }?> ><?php echo $value; ?></option>
+                          <?php } ?>
+                        </select>
+                      </div><!-- /.input group -->
+                    </div><!-- /.form group -->
+                    <div class="form-group">
+                      <label for="date">Quantity</label>
+                      <div class="input-group col-md-2">
+                        <input type="number" class="form-control pull-right" max='150' value="<?php echo $qty;?>" name="qty" placeholder="Quantity" required>
+                      </div><!-- /.input group -->
+                    </div><!-- /.form group -->            
+                    <div class="form-group">
+                      <div class="input-group">
+                        <button class = "btn btn-primary" id="btn_submit">Submit</button>
+                      </div>
+                    </div><!-- /.form group -->
+                    </form>	
+                  </div><!-- /.box-body -->
+                </div><!-- /.box -->
+              </div><!-- /.col (right) -->
+            </div><!-- /.row -->
+	  
+            
+          </section><!-- /.content -->
+        </div><!-- /.container -->
+      </div><!-- /.content-wrapper -->
+      <?php include('../dist/includes/footer.php');?>
+    </div><!-- ./wrapper -->
+
+    <!-- jQuery 2.1.4 -->
+    <script src="../plugins/jQuery/jQuery-2.1.4.min.js"></script>
+    <!-- Bootstrap 3.3.5 -->
+    <script src="../bootstrap/js/bootstrap.min.js"></script>
+    <!-- SlimScroll -->
+    <script src="../plugins/slimScroll/jquery.slimscroll.min.js"></script>
+    <!-- FastClick -->
+    <script src="../plugins/fastclick/fastclick.min.js"></script>
+    <!-- AdminLTE App -->
+    <script src="../dist/js/app.min.js"></script>
+    <!-- AdminLTE for demo purposes -->
+    <script src="../dist/js/demo.js"></script>
+    <script src="../plugins/datatables/jquery.dataTables.min.js"></script>
+    <script src="../plugins/datatables/dataTables.bootstrap.min.js"></script>
+    
+    <script>
+      $(function () {
+        $("#example1").DataTable();
+        $('#example2').DataTable({
+          "paging": true,
+          "lengthChange": false,
+          "searching": false,
+          "ordering": true,
+          "info": true,
+          "autoWidth": false
+        });
+      });
+      // ('#formE').on('keyup keypress', function(e) {
+      //   var keyCode = e.keyCode || e.which;
+      //   if (keyCode === 13) { 
+      //     e.preventDefault();
+      //     return false;
+      //   }
+      // });
+      $("#formE").keypress(function(e) {
+        //Enter key
+        if (e.which == 13) {
+          return false;
+        }
+      });
+
+      $("#btn_submit").click(function () {
+          tot = 1;
+          // ok = myFunction();
+
+          // if(ok == true){
+          //   tot += 1;
+          // }
+          //  alert("tot is : " + tot); 
+          // if(tot>1){
+          document.getElementById("formE").submit();
+          // }
+      });
+
+      function myFunction() {
+        var pass1 = document.getElementById("model").value;
+        var pass2 = document.getElementById("model_no").value;
+        var pass_old = document.getElementById("qty").value;
+        var ok = true;
+        
+        if(pass_old.trim() == ""){
+          alert("Please enter your current password");
+          ok = false;
+        }
+        if ((pass1 != null || pass2 != null ) && pass1 != pass2) {
+            alert("Passwords Do not match");
+            document.getElementById("password").style.borderColor = "#E34234";
+            document.getElementById("cfmPassword").style.borderColor = "#E34234";
+            ok = false;
+        }
+        return ok;
+
+      }
+
+      function validateEmail(email) {
+        var re = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+        return re.test(email);
+      }
+
+	</script>
+  </body>
+</html>
---
+++ pages/sncheck_in.php
@@ -0,0 +1,288 @@
+<?php 
+
+include('../dist/includes/dbcon.php');
+include('product_cfg.php');
+	$box_id = $_POST['box_id'];
+	$qty = $_POST['qty'];
+	$no = $_POST['model'];
+	$id = $_SESSION['id'];
+	$tmstmp = time(); 
+
+	$allmodel = get_model($con);
+	$allmodelNo = get_modelNo($con);
+	$allmodelNo2 = get_modelNo2($con);
+	$allmodel_name = get_model_name($con);
+	$model = $allmodel[$no];
+	$model_no = $allmodelNo[$no];
+	$model_name = $allmodel_name[$no];
+	$model_no2 = $allmodelNo2[$no];
+
+	if((strpos($model_no,"M009249.1")!== false) ){//for K5 Key just proceed to print
+		//Check for duplicate SN
+		$dupmsg = 'Duplicate SN detected in: \n';
+		$dupmsg_T = 0;
+		for($i=1;$i<=$qty;$i++){
+			$dup_detect = 0;
+			for($c=1;$c<=$qty;$c++){
+				if($c!=$i){
+					$_POST['sn'.$i]==$_POST['sn'.$c]?$dup_detect=1:$dup_detect=$dup_detect;
+				}
+			}
+			$dup_detect==1?$dupmsg.='-line '.$i.'\n':$dupmsg = $dupmsg;
+			$dup_detect==1?$dupmsg_T = 1: $dupmsg_T=$dupmsg_T;
+		}
+		if($dupmsg_T==1){
+			echo '<script type="text/javascript">alert("'.$dupmsg.'");</script>';
+			echo "<script>window.history.back();</script>"; 
+		}
+		else{
+
+			$invalidmsg = '';
+			$invalidsn = 0;
+			for($i=1;$i<=$qty;$i++){
+				$sn = $_POST['sn'.$i];
+				if(strlen($sn)>9 && strlen($sn)<20){
+					$invalidsn = 1;
+					$invalidmsg.=$sn.' on line '.$i.' is invalid!\n';
+				}
+			}
+			if($invalidsn){
+				echo '<script type="text/javascript">alert("'.$invalidmsg.'");</script>';
+				echo "<script>window.history.back();</script>"; 
+			}
+			else{
+				//check for existing data
+				$existmsg='';
+				$testmsg='';
+				$existed = 0;
+				$testfailed=0;
+				for($i=1;$i<=$qty;$i++){
+					$sn= $_POST['sn'.$i];
+					$query=mysqli_query($con,"select count(*) as cnt from box_sn where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']!=0){
+						$existed = 1;
+						$existmsg.=$sn.' on line '.$i.' already exist in the system!\n';
+					}
+				}
+				if($existed){
+					echo '<script type="text/javascript">alert("'.$existmsg.'");</script>';
+					echo "<script>window.history.back();</script>"; 
+				}
+				else{
+					
+					echo "<script type='text/javascript'>alert('All SN is OKAY!');</script>";
+					echo "<script>window.history.back();</script>"; 
+					
+				}
+			}
+		}
+	}
+	else{
+	
+	$printlbl = 1;
+	$model_no=="IQ-M009453-J-00"?$printlbl=0:$printlbl=1;
+	//Check for duplicate SN
+	$dupmsg = 'Duplicate SN detected in: \n';
+    $dupmsg_T = 0;
+    for($i=1;$i<=$qty;$i++){
+        $dup_detect = 0;
+        for($c=1;$c<=$qty;$c++){
+            if($c!=$i){
+                $_POST['sn'.$i]==$_POST['sn'.$c]?$dup_detect=1:$dup_detect=$dup_detect;
+            }
+        }
+        $dup_detect==1?$dupmsg.='-line '.$i.'\n':$dupmsg = $dupmsg;
+        $dup_detect==1?$dupmsg_T = 1: $dupmsg_T=$dupmsg_T;
+    }
+    if($dupmsg_T==1){
+        echo '<script type="text/javascript">alert("'.$dupmsg.'");</script>';
+        echo "<script>window.history.back();</script>"; 
+	}
+	else{
+		//check for existing data
+		$existmsg='';
+		$testmsg='';
+		$existed = 0;
+		$testfailed=0;
+		for($i=1;$i<=$qty;$i++){
+			$sn= $_POST['sn'.$i];
+			
+			$query=mysqli_query($con,"select count(*) as cnt from box_sn where sn='$sn'")or die(mysqli_error($con));
+			$row=mysqli_fetch_array($query);
+			if($row['cnt']!=0){
+				$existed = 1;
+				$existmsg.=$sn.' on line '.$i.' already exist in the system!\n';
+			}
+		}
+		if($existed){
+			echo '<script type="text/javascript">alert("'.$existmsg.'");</script>';
+        	echo "<script>window.history.back();</script>"; 
+		}
+		else{
+
+			for($i=1;$i<=$qty;$i++){
+				$rfsissue = 0;
+				$sn= $_POST['sn'.$i];
+				if(strpos($model_name,"NFC")!== false){//temporary for NFC 
+					$query=mysqli_query($con,"select result,fdate, count(*) as cnt from nfc_test where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']==0 || $row['result']!="P"){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass NFC Core test yet!.\n';
+					}
+				}
+				elseif(strpos($model_no,"IQ-M011795")!== false){//for D5  Core
+					$printlbl = 0;
+					$query=mysqli_query($con,"select result,count(*) as cnt from d5_burn where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']==0 || ($row['result']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Burn-in test yet!.\n';
+					}
+				}
+				elseif(strpos($model_no,"IQ-M011793")!== false){//for D5 FG
+					$printlbl = 0;
+					$query=mysqli_query($con,"select result,count(*) as cnt from d5_durability where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']==0 || ($row['result']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Durability test yet!.\n';
+					}
+
+					if($tempres = checkTempTest($sn,$con)){
+						if($tempres['result']!="P"){
+							$testfailed = 1;
+							$testmsg.=$sn.' on line '.$i.' not pass Temperature test yet.\n';
+						}
+					}
+					else{
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Temperature test yet.\n';
+					}
+				}
+				elseif(strpos($model_name,"SKOGEN KEY")!== false){//temporary for skogen  
+					$printlbl = 0;
+					$query=mysqli_query($con,"select lockTest,lDate, count(*) as cnt from sn_master where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']==0 || ($row['lockTest']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Lock test yet!.\n';
+					}
+				}
+				else{
+					$query=mysqli_query($con,"select lockTest, durTest, rfsTest,lDate,rDate,dDate, count(*) as cnt from sn_master where sn='$sn'")or die(mysqli_error($con));
+					$row=mysqli_fetch_array($query);
+					if($row['cnt']==0 || ($row['lockTest']!="P"&&$row['durTest']!="P"&&$row['rfsTest']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass ANY test yet!.\n';
+					}
+					elseif(($row['lockTest']=="P"&&$row['durTest']!="P"&&$row['rfsTest']=="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Durability test yet!\n';
+					}
+					elseif(($row['lockTest']!="P"&&$row['durTest']=="P"&&$row['rfsTest']=="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Lock test yet!\n';
+					}
+					elseif(($row['lockTest']=="P"&&$row['durTest']=="P"&&$row['rfsTest']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass RFS test yet!\n';
+					}
+					elseif(($row['lockTest']!="P"&&$row['durTest']!="P"&&$row['rfsTest']=="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Lock & Durability test yet!\n';
+					}
+					elseif(($row['lockTest']!="P"&&$row['durTest']=="P"&&$row['rfsTest']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Lock & RFS test yet!\n';
+					}
+					elseif(($row['lockTest']=="P"&&$row['durTest']!="P"&&$row['rfsTest']!="P")){
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass RFS & Durability test yet!\n';
+					}
+					elseif(($row['rDate']<=$row['dDate']||$row['rDate']<=$row['dDate'])){
+						$testfailed = 1;
+						$rfsissue = 1;
+						$testmsg.=$sn.' on line '.$i.' RFS Test is not valid! Please start over test process.\n';
+					}
+				}
+
+				if(strpos($model_name,"OUTDOOR")){
+
+					if($tempres = checkTempTest($sn,$con)){
+						$rdate = strtotime($row['rDate']);
+						if($tempres['result']!="P"){
+							$testfailed = 1;
+							$testmsg.=$sn.' on line '.$i.' not pass Temperature test yet.\n';
+						}
+						elseif($tempres['time_out']<$rdate && $rfsissue==0){
+							$testfailed = 1;
+							$testmsg.=$sn.' on line '.$i.' RFS Test is not valid! Please start over test process.\n';
+						}
+					}
+					else{
+						$testfailed = 1;
+						$testmsg.=$sn.' on line '.$i.' not pass Temperature test yet.\n';
+					}
+
+				}
+				
+			}
+			if($testfailed){
+				echo '<script type="text/javascript">alert("'.$testmsg.'");</script>';
+				echo "<script>window.history.back();</script>"; 
+			}
+			else{
+			
+			
+				
+				echo "<script type='text/javascript'>alert('All data is OKAY!');</script>";
+				echo "<script>window.history.back();</script>"; 
+				
+				
+			}
+		}
+	}
+
+		
+	}
+
+	function checkTempTest($sn,$con){
+		
+        $query=mysqli_query($con,"SELECT tt.id, tt.temperature, ts.sn, tt.time_out, ts.result
+        FROM temp_test tt
+        JOIN temp_test_sn ts ON tt.id = ts.batch_id
+        WHERE ts.sn = $sn AND ts.result IS NOT NULL
+        ORDER BY tt.id DESC")or die(mysqli_error($con));
+
+        $rowcount=mysqli_num_rows($query);
+		$retval = array();
+        if($rowcount>=1){
+
+            while($row = mysqli_fetch_assoc($query)) {
+
+                if($row['temperature']==1){
+					$timeout = date("YmdHis", $retval['time_out']);
+                    isset($retval['result'])?$retval['result']=$retval['result']:$retval['result']=$row['result'];
+                    isset($retval['time_out'])?$retval['time_out']=$retval['time_out']:$retval['time_out']=$timeout;
+                }
+
+                // if($row['temperature']==2){
+					// $timeout = date("YmdHis", $retval['time_out']);
+                //     isset($retval['result2'])?$retval['result2']=$retval['result2']:$retval['result2']=$row['result'];
+                //     isset($retval['time_out2'])?$retval['time_out2']=$retval['time_out2']:$retval['time_out2']=$timeout;
+                // }
+            }
+        }
+        else{            
+            $retval['result'] = '';
+            $retval['time_out'] ='';
+            // $retval['result2'] = '';
+            // $retval['time_out2'] ='';
+        }
+
+		return $retval;
+	}
+	
+?>

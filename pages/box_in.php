0fd30faf6516a103c795f74ba222b1552bace1e0
muhdfirdaus <dausmomo5779@gmail.com>
2020-12-07 15:36:46 +0800

    fix error regarding label not printing out

--- pages/box_in.php
+++ pages/box_in.php
@@ -97,7 +97,7 @@ include('product_cfg.php');
 					mysqli_query($con, "UPDATE box_info set status=1 where box_id ='$box_id'")or die(mysqli_error($con));
 					echo "<script type='text/javascript'>alert('Data saved!');</script>";
 					
-					$query=mysqli_query($con,"select ip from printer_cfg where name=Box$line")or die(mysqli_error($con));
+					$query=mysqli_query($con,"select ip from printer_cfg where name='Box$line'")or die(mysqli_error($con));
 					$row=mysqli_fetch_array($query);
 					$ip=$row['ip'];
 
@@ -649,7 +649,7 @@ include('product_cfg.php');
 				mysqli_query($con, "UPDATE box_info set status=1 where box_id ='$box_id'")or die(mysqli_error($con));
 				echo "<script type='text/javascript'>alert('Data saved!');</script>";
 				
-				$query=mysqli_query($con,"select ip from printer_cfg where name=Box$line")or die(mysqli_error($con));
+				$query=mysqli_query($con,"select ip from printer_cfg where name='Box$line'")or die(mysqli_error($con));
 				$row=mysqli_fetch_array($query);
 				$ip=$row['ip'];
 

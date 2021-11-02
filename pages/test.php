260a7a54764a40f5989bdc39740e97219b950fbd
muhdfirdaus <dausmomo5779@gmail.com>
2021-11-02 12:39:11 +0800

    test

--- pages/test.php
+++ pages/test.php
@@ -1,20 +1,14 @@
 <?php 
-$payment_for = date("Y-m-d",strtotime("2016-1-20"));
-$date = date("Y-m-d");
-			
-                        $due1 = date("Y-m-d",strtotime($payment_for. " +1 months"));
-						$due2 = date("Y-m-d",strtotime($due1. " +5 days"));
-						
-                        if ($date > $due2)
-                          {
-                            $interest=100*.05;
-                            
-                          } 
-                         else
-                          {
-                            $interest=0;
-                            
-                          }
-						 echo "<br>";
-						  echo $interest;
-		?>
+
+						//function to print second label
+						$filename = "lbliloq_prt.txt";
+						$file = fopen($filename, "r+")or die("ERROR: Cannot open the file .")  ;
+						if($file){
+							fwrite($file, $lblbox2);      
+							fclose($file);
+						} 
+
+						//print second label
+						copy($filename, "//BTS-iLOQ-1/iloqpizza"); 
+
+?>
